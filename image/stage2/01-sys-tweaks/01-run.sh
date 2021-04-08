535486c4f4a121d521b7641f1037fa583584ea0a
mkellsy <mkellsy@gmail.com>
2021-04-08 16:02:53 -0600

    install gpiozero

--- image/stage2/01-sys-tweaks/01-run.sh
+++ image/stage2/01-sys-tweaks/01-run.sh
@@ -21,14 +21,14 @@
 
 install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
 
-install -d				"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
-install -m 644 files/ttyoutput.conf	"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
+install -d "${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
+install -m 644 files/ttyoutput.conf "${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
 
-install -m 644 files/50raspi		"${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+install -m 644 files/50raspi "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
 
-install -m 644 files/console-setup   	"${ROOTFS_DIR}/etc/default/"
+install -m 644 files/console-setup "${ROOTFS_DIR}/etc/default/"
 
-install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
+install -m 755 files/rc.local "${ROOTFS_DIR}/etc/"
 
 if [ -n "${PUBKEY_SSH_FIRST_USER}" ]; then
 	install -v -m 0700 -o 1000 -g 1000 -d "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.ssh
@@ -68,11 +68,11 @@ EOF
 fi
 
 on_chroot <<EOF
-    for GRP in input spi i2c; do
+    for GRP in input spi i2c gpio; do
     	groupadd -f -r "\$GRP"
     done
 
-    for GRP in adm dialout cdrom audio users sudo video plugdev input spi i2c netdev; do
+    for GRP in adm dialout cdrom audio users sudo video plugdev input gpio spi i2c netdev; do
         adduser $FIRST_USER_NAME \$GRP
     done
 EOF
